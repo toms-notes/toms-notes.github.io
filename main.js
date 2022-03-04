488644fa55554b5b588acf3763670c31e47ad677
toms-notes <100950395+toms-notes@users.noreply.github.com>
2022-03-04 17:02:49 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function AddQeds() {
-  var qedElems = document.getElementsByClassName('qed');
+  var qedElems = document.getElementsByTagName('qed');
   for (var i = 0; i < qedElems.length; ++i) {
     var item = qedElems[i];
     item.insertAdjacentHTML('afterBegin', "❖");
