5f5879411472b41fe968693fd6daa0a2b6152f4d
toms-notes <100950395+toms-notes@users.noreply.github.com>
2022-03-04 16:55:52 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function ProofsFunctionality() {
+function AddQeds() {
   var qedElems = document.getElementsByClassName('qed');
   for (var i = 0; i < qedElems.length; ++i) {
     var item = qedElems[i];  
