e0f809a18d38314e7f2307b89a6b0a2e045da3f5
toms-notes <100950395+toms-notes@users.noreply.github.com>
2022-03-04 16:58:09 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function AddQeds() {
   var qedElems = document.getElementsByClassName('qed');
   for (var i = 0; i < qedElems.length; ++i) {
-    var item = qedElems[i];  
-    item.innerHTML = '❖';
+    var item = qedElems[i];
+    item.insertAdjacentHTML('afterBegin', "❖");
   }
 }
